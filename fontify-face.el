5bbd4bbbb69f3fe156af53d32000a4b769ed4cbd
Matus Goljer <matus.goljer@gmail.com>
2018-04-10 16:06:52 +0200

    Release 1.0.0

--- fontify-face.el
+++ fontify-face.el
@@ -4,7 +4,7 @@
 
 ;; Author: Matúš Goljer <matus.goljer@gmail.com>
 ;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
-;; Version: 0.0.1
+;; Version: 1.0.0
 ;; Created: 10th April 2018
 ;; Package-requires: ()
 ;; Keywords: faces
