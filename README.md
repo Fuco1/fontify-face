3c3e9ef4de91246aeb7bf1e992bc3094176c492f
Matus Goljer <matus.goljer@gmail.com>
2018-04-10 16:05:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# fontify-face
+
+Fontify symbols representing faces with that face.
+
+# Usage
+
+Enable `fontify-face-mode`.
+
+# Rationale
+
+During theme or package development it is useful to see how the faces
+look directly during development.  This saves time and provides
+thighter feedback loop.  Happy theming!
+
+![](https://i.imgur.com/MqBpJou.png)
---
+++ fontify-face.el
@@ -0,0 +1,61 @@
+;;; fontify-face.el --- Fontify symbols representing faces with that face. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Matúš Goljer
+
+;; Author: Matúš Goljer <matus.goljer@gmail.com>
+;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
+;; Version: 0.0.1
+;; Created: 10th April 2018
+;; Package-requires: ((dash "2.10.0"))
+;; Keywords: faces
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(defun fontify-face-colorize-matched-face ()
+  "Fontify the last matched region.
+
+The face used to fontify the region is the text of the region if
+it represents a face, otherwise nothing happens."
+  (let ((match (match-string 0)))
+    (when (facep match) match)))
+
+(defun fontify-face-find-next-symbol (limit)
+  "Find the next symbol up until LIMIT."
+  (re-search-forward
+   (rx symbol-start
+       (1+ (or (syntax symbol) (syntax word)))
+       symbol-end)
+   limit t))
+
+(defconst fontify-face-keywords
+  `((fontify-face-find-next-symbol 0 (funcall 'fontify-face-colorize-matched-face)))
+  "Keywords used for highlighting faces.
+
+Note: instead of using constants we use functions which are not
+expected to change very much.  This is to make toggling the
+display somewhat reliable during updates.")
+
+(define-minor-mode fontify-face-mode
+  "Fontify symbols representing faces with that face."
+  :lighter " 💡"
+  (if fontify-face-mode
+      (font-lock-add-keywords nil fontify-face-keywords)
+    (font-lock-remove-keywords nil fontify-face-keywords)))
+
+(provide 'fontify-face)
+;;; fontify-face.el ends here
